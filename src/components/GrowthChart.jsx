1be0a88c9f9296f29478467b297af8fc094c2480
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-05-31 22:37:50 +0530

    newq results added

--- src/components/GrowthChart.jsx
+++ src/components/GrowthChart.jsx
@@ -7,11 +7,11 @@ ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, T
 
 const GrowthChart = () => {
   const data = {
-    labels: ['2020', '2021', '2022'],
+    labels: ['2020', '2021', '2022', '2023'],
     datasets: [
       {
         label: 'Physics',
-        data: [73.905, 98.437, 98.9275],
+        data: [73.905, 98.437, 98.9275, 100],
         borderColor: '#FF6384',
         pointRadius: 7,
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
@@ -19,21 +19,21 @@ const GrowthChart = () => {
       },
       {
         label: 'Chemistry',
-        data: [70, 90, 100],
+        data: [70, 90, 100, 100],
         borderColor: '#36A2EB',
         pointRadius: 7,
         backgroundColor: 'rgba(54, 162, 235, 0.2)',
       },
       {
         label: 'Combined Maths',
-        data: [60, 80, 95],
+        data: [60, 80, 95, 88.89],
         borderColor: '#FFCE56',
         pointRadius: 7,
         backgroundColor: 'rgba(255, 206, 86, 0.2)',
       },
       {
         label: 'Biology',
-        data: [65, 85, 100],
+        data: [65, 85, 100, 94.11],
         borderColor: '#4BC0C0',
         pointRadius: 7,
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
@@ -57,7 +57,7 @@ const GrowthChart = () => {
       },
       title: {
         display: false,
-        text: 'Pass Percentage Growth Over Three Batches (2020-2022)',
+        text: 'Pass Percentage Growth Over Three Batches (2020-2023)',
         font: {
           size: 32,
           family: 'Chakra Petch, sans-serif',
@@ -119,7 +119,7 @@ const GrowthChart = () => {
 
   return <div className='section chart' data-aos="fade-up">
     <h1 className='.section-title'>
-    Pass Percentage Growth Over Three Batches (2020-2022)
+    Pass Percentage Growth Over Three Batches (2020-2023)
     </h1>
     <Line data={data} options={options} />
     </div>;
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -14,18 +14,18 @@ export const News = () => {
                             <p>We are proud to announce that our science project OSDEM has been featured on EverybodyWiki. Click <a href="https://en.everybodywiki.com/OSDEM-DFEC_Science_Project" target="_blank" rel="noreferrer"><b>here</b></a> to read more.</p>
                         </div>
                     </div> */}
+                    <div className='news-item'>
+                        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2FDFEC.DIKWELLA%2Fposts%2Fpfbid0LCKtAreSJ6egZzVG49AS1rtPd8dee3Zxc8hFwwMcXfUvu1CLDXbgdkn4Wux1qkiel&show_text=true&width=500&preview=comet_preview" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                        <div className="news-info">
+                            <h3>2023 A/L Batch Results Are Out!</h3>
+                            <p>Our 2023 A/L batch has achieved excellent results. Congratulations to all the students!</p>
+                        </div>
+                    </div>
                     <div className="news-item">
                         <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2FDFEC.DIKWELLA%2Fposts%2Fpfbid02Q5Hn1fM6MZDRwGFR4LNTSUK6WKTSWziB5RsJDoCEBY4pLgqUWpgwKcrpUK3DEgHwl&show_text=true&width=500&is_preview=true" scrolling="no" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                         <div className="news-info">
                             <h3>2026 A/L Batch Applications Now Open</h3>
                             <p>Applications for the 2026 A/L batch are now open. Apply now to join our transformative educational program.</p>
-                        </div>
-                    </div>
-                    <div className="news-item">
-                        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2FDFEC.DIKWELLA%2Fposts%2Fpfbid0ND4dgofVw4f3knZmCjkXT6dp1cfyR8pzso5Va3dwWPsQXhiajuM6t3eQuhPiHg5Ml&show_text=true&width=500&is_preview=true" scrolling="no" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-                        <div className="news-info">
-                            <h3>2022(2023) University admissions with first waiting of our 3rd batch are out! </h3>
-                            <p>Our 3rd batch of students have been admitted to universities. Congratulations to all the students!</p>
                         </div>
                     </div>
                 </div>
